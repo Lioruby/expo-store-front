763c3fdefc62a575f789fa13b8cae393742b9019
Lioruby <levy.lior.pro@gmail.com>
2024-08-02 17:18:16 +0200

    task: remove android implementation

--- android/src/main/java/expo/modules/storefront/ExpoStoreFrontModule.kt
+++ android/src/main/java/expo/modules/storefront/ExpoStoreFrontModule.kt
@@ -1,46 +1,6 @@
 package expo.modules.storefront
 
 import expo.modules.kotlin.modules.Module
-import expo.modules.kotlin.modules.ModuleDefinition
-import com.android.billingclient.api.*
-import android.content.Context
 
 class ExpoStoreFrontModule : Module() {
-  private lateinit var billingClient: BillingClient
-
-  override fun definition() = ModuleDefinition {
-    Name("ExpoStoreFrontModule")
-
-    Function("getCountryCode") {
-      getCountryCode()
-    }
-
-    AsyncFunction("initializeBillingClient") {
-      initializeBillingClient()
-    }
-  }
-
-  private fun initializeBillingClient() {
-    val context: Context = appContext.reactContext ?: throw IllegalStateException("React context is null")
-    billingClient = BillingClient.newBuilder(context).build()
-  }
-
-  private fun getCountryCode(): String {
-    var countryCode = "Unknown"
-    val getBillingConfigParams = GetBillingConfigParams.newBuilder().build()
-    billingClient.getBillingConfigAsync(getBillingConfigParams,
-      object : BillingConfigResponseListener {
-        override fun onBillingConfigResponse(
-            billingResult: BillingResult,
-            billingConfig: BillingConfig?
-        ) {
-            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK
-                && billingConfig != null) {
-                countryCode = billingConfig.countryCode
-            }
-        }
-    })
-
-    return countryCode
-  }
 }
